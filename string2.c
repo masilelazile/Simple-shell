8460050f215b3157e7f84ee7c82a97ae4373205a
Thokozile Masilela <123003591+masilelazile@users.noreply.github.com>
2023-07-28 23:57:16 +0200

    Update string2.c

--- string2.c
+++ string2.c
@@ -5,4 +5,37 @@
 * @dest: the destination buffer
 * @src: the source buffer
 *
+* Return: pointer to destination buffer
+*/
+
+char *_strcat(char *dest, char *src)
+{
+  char *ret = dest;
+  while (*dest)
+    dest++
+      while (*src)
+    *dest++ = *src++;
+    *dest = *src;
+return (ret);
+
+/**
+* _strdup - duplicates a string
+* @str: the string to duplicate
 *
+* Return: pointer to the duplicated string
+*/
+char *_strdup(const char *str)
+{
+  int length = 0;
+  char *ret;
+  if (str == NULL)
+    return (NUUL);
+while (*str++)
+  length++;
+ret = malloc(sizeof(char) * (length + 1));
+if (!ret)
+  return (NULL);
+for (length++; length--;)
+  ret[length] = *--str;
+return (ret);
+}
