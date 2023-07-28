c91b90c094a5a62827a50665f8e518a0f9a89c4c
Thokozile Masilela <123003591+masilelazile@users.noreply.github.com>
2023-07-29 01:16:08 +0200

    simple shell

--- string2.c
+++ string2.c
@@ -1,66 +1,55 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * _strcat - Concatenates two strings
- * @dest: The destination buffer
- * @src: The source buffer
- *
- * Return: Pointer to destination buffer
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * Return: pointer to destination buffer
  */
 
 char *_strcat(char *dest, char *src)
 {
 	char *ret = dest;
-
+	
 	while (*dest)
 	{
 		dest++;
 	}
-
 	while (*src)
 	{
 		*dest++ = *src++;
 	}
-
-	*dest = '\0';
+	*dest = *src;
 	return ret;
 }
 
 /**
- * _strdup - Duplicates a string
- * @str: The string to duplicate
- *
- * Return: Pointer to the duplicated string
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ * Return: pointer to the duplicated string
  */
 
-char *_strdup(const char *str)
+char *_strdup(const char *str) 
 {
 	int length = 0;
 	char *ret;
-
-	if (str == NULL)
+	
+	if (str == NULL) 
 	{
 		return NULL;
 	}
-
-	while (str[length])
+	while (*str++) 
 	{
 		length++;
 	}
-
 	ret = malloc(sizeof(char) * (length + 1));
 	if (!ret)
 	{
 		return NULL;
 	}
-
-	for (length = 0; str[length]; length++)
+	for (length++; length--;) 
 	{
-		ret[length] = str[length];
+		ret[length] = *--str;
 	}
-
-	ret[length] = '\0';
 	return ret;
 }
